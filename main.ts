e8053039c5233f88f5d1b5478f68ac29a7635e49
Bubbah69 <Bubbah69@users.noreply.github.com>
2021-11-09 14:21:07 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,37 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
-})
 input.onButtonPressed(Button.A, function () {
-    radio.sendString(">>")
+    radio.sendString("L")
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # . . .
+        . . # . .
+        . . . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("B")
+    basic.showIcon(IconNames.Sword)
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(1)
+    radio.sendString("R")
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("S")
+    basic.showIcon(IconNames.No)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendNumber(input.rotation(Rotation.Roll))
+    radio.sendString("F")
+    basic.showIcon(IconNames.Triangle)
 })
 radio.setGroup(1)
 basic.forever(function () {
